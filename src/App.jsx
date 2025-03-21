f009f5749d18254d86269ae63b92ee449706ce65
Kushagra Upadhyay <kushagraupadhyay926@gmail.com>
2025-03-21 17:44:33 +0530

    saved the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
 
 import './App.css'
 import CardComponent from './CardComponent'
+import Header from './Header'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './Routes/About';
+import Contact from './Routes/Contact';
+import Product from './Routes/Product';
+import Home from './Routes/Home';
 
 function App() {
- 
+
+  const router = createBrowserRouter([
+    {
+    path:'/',
+    element: <Home />
+    },
+    {
+    path:'about',
+    element: <About />
+    },
+    {
+    path: 'contact us',
+    element: <Contact />,
+    },
+    {
+    path: 'products',
+    element: <Product />,
+    }
+  ])
 
   return (
     <>
+    <Header/>
+    <div className="card">
     <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    </div>
+    <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <nav>
+                <ul>
+                <li><p>Home</p></li>
+                <li><p>About</p></li>
+                <li><p>Contact Us</p></li>
+                <li><p>Products</p></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Header
